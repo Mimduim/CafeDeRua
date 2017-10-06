3ba40dbd4de63d3902e95fe0231f72585238d9d8
Paulo Henrique Rodrigues <paulohenriquercsh@outlook.com>
2017-10-06 03:41:01 -0300

    Pequenas modificações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import 'materialize-css';
+
 import { MaterializeModule } from 'angular2-materialize';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
